2d9414f9aedf98cf3c808123e5e0d5d66402298e
Spencer Rawls <rawlsspencer@gmail.com>
2018-09-19 21:05:36 -0500

    Implemented chests.

---
+++ Spire of Remembrance/Assets/Scripts/TreasureChest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : Interactable
+{
+	[SerializeField] private Sprite closedSprite;
+	[SerializeField] private Sprite openSprite;
+	[SerializeField] private GameObject contentsPrefab;
+
+	new private SpriteRenderer renderer;
+
+	public override string interactionText
+	{
+		get
+		{
+			return "Open";
+		}
+	}
+
+	private void Awake()
+	{
+		renderer = GetComponent<SpriteRenderer>();
+		renderer.sprite = closedSprite;
+	}
+
+	public override Coroutine Interact(GameObject interacter)
+	{
+		renderer.sprite = openSprite;
+		Instantiate(contentsPrefab, transform.position, Quaternion.identity);
+		return StartCoroutine(dummyCoroutine());
+	}
+
+	private IEnumerator dummyCoroutine()
+	{
+		Destroy(this);
+		yield break;
+	}
+}
