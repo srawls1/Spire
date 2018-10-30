9ae9fd5b53922ccb37762d97750717e71c71ea30
Spencer Rawls <rawlsspencer@gmail.com>
2018-10-29 22:30:25 -0500

    Laid groundwork for rat tunnel movement.

---
+++ Spire of Remembrance/Assets/Scripts/RatTunnel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatTunnel : MonoBehaviour
+{
+	[SerializeField] private RatTunnelNode[] endPoints;
+
+	public bool IsAtEntry(Vector2 position)
+	{
+		return getEntryIndex(position) >= 0;
+	}
+
+	public RatTunnelNode GetEntryNode(Vector2 position, Vector2 direction)
+	{
+		int entryIndex = getEntryIndex(position);
+		if (entryIndex < 0)
+		{
+			return null;
+		}
+
+		if (endPoints[entryIndex].ClosestDirectionIndex(direction) >= 0)
+		{
+			return endPoints[entryIndex];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public bool IsExiting(Vector2 position, Vector2 direction)
+	{
+		int entryIndex = getEntryIndex(position);
+		if (entryIndex < 0)
+		{
+			return false;
+		}
+
+		return endPoints[entryIndex].ClosestDirectionIndex(direction) < 0;
+	}
+
+	public Vector2 MoveInDirection(ref RatTunnelNode currentNode, Vector2 position, Vector2 movement)
+	{
+		// Handle entry
+		if (currentNode == null)
+		{
+			currentNode = GetEntryNode(position, movement);
+			if (currentNode == null)
+			{
+				return position;
+			}
+		}
+
+		// Handle exit
+		if (IsExiting(position, movement))
+		{
+			currentNode = null;
+			return position;
+		}
+
+		Vector2 nodePosition = currentNode.transform.position;
+
+		// Handle being at a node
+		if (currentNode.IsInNode(position))
+		{
+			int connectionIndex = currentNode.ClosestDirectionIndex(movement);
+			if (connectionIndex < 0)
+			{
+				return position;
+			}
+
+			position += movement;
+			float disp = Vector2.Dot(position - nodePosition, currentNode.GetConnectingDirection(connectionIndex));
+			return currentNode.GetTowardConnectionPosition(connectionIndex, disp);
+		}
+		// Handle being between nodes
+		else
+		{
+			Vector2 disp = position - nodePosition;
+			float dist = disp.magnitude;
+			int connectionIndex = currentNode.ClosestDirectionIndex(disp);
+			Vector2 dir = currentNode.GetConnectingDirection(connectionIndex);
+			dist += Vector2.Dot(movement, dir);
+			Vector2 newPosition = currentNode.GetTowardConnectionPosition(connectionIndex, dist);
+
+			if (currentNode.IsInConnectedNode(newPosition, connectionIndex))
+			{
+				currentNode = currentNode.GetConnectedNode(connectionIndex);
+			}
+
+			return newPosition;
+		}
+	}
+
+	private int getEntryIndex(Vector2 position)
+	{
+		for (int i = 0; i < endPoints.Length; ++i)
+		{
+			if (endPoints[i].IsInNode(position))
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+}
---
+++ Spire of Remembrance/Assets/Scripts/RatTunnelNode.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatTunnelNode : MonoBehaviour
+{
+	[SerializeField] private float distancePermission;
+	[SerializeField] private RatTunnelNode[] connectingNodes;
+
+	public bool IsInNode(Vector2 position)
+	{
+		return GetDistanceFrom(position) < distancePermission;
+	}
+
+	public bool IsInConnectedNode(Vector2 position, int connectionIndex)
+	{
+		if (connectionIndex < 0 || connectionIndex > connectingNodes.Length)
+		{
+			return false;
+		}
+
+		return connectingNodes[connectionIndex].IsInNode(position);
+	}
+
+	public RatTunnelNode GetConnectedNode(int connectionIndex)
+	{
+		if (connectionIndex < 0 || connectionIndex > connectingNodes.Length)
+		{
+			return null;
+		}
+
+		return connectingNodes[connectionIndex];
+	}
+
+	public float GetDistanceFrom(Vector2 position)
+	{
+		return Vector2.Distance(transform.position, position);
+	}
+
+	// Returns the index of the connecting node whose direction from
+	// this node is closest to the direction of the given vector
+	public int ClosestDirectionIndex(Vector2 direction)
+	{
+		int closestIndex = -1;
+		float maxDotProduct = direction.magnitude / Mathf.Sqrt(2f); // This corresponds to a maximum angle of 45 deg
+
+		for (int i = 0; i < connectingNodes.Length; ++i)
+		{
+			Vector2 dir = GetConnectingDirection(i);
+			float dotProduct = Vector2.Dot(direction, dir);
+			if (dotProduct > maxDotProduct)
+			{
+				maxDotProduct = dotProduct;
+				closestIndex = i;
+			}
+		}
+
+		return closestIndex;
+	}
+
+	public Vector2 GetConnectingDirection(int connectionIndex)
+	{
+		if (connectionIndex < 0 || connectionIndex > connectingNodes.Length)
+		{
+			throw new ArgumentException("Invalid connection index: " + connectionIndex);
+		}
+
+		Vector2 diff = connectingNodes[connectionIndex].transform.position -
+			transform.position;
+		return diff.normalized;
+	}
+
+	public Vector2 GetTowardConnectionPosition(int connectionIndex, float displacement)
+	{
+		Vector2 direction = GetConnectingDirection(connectionIndex);
+		float maxDistance = Vector2.Distance(transform.position,
+			connectingNodes[connectionIndex].transform.position);
+		Vector2 startingPosition = transform.position;
+		return startingPosition + direction * Mathf.Clamp(displacement, 0f, maxDistance);
+	}
+}
