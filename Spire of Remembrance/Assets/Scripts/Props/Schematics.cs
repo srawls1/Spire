991eb141de5d57192eb3eecc31e6c3b721e37e73
Spencer Rawls <rawlsspencer@gmail.com>
2019-01-07 21:47:16 -0600

    Implemented the map screen, scribes/cartographers, and floor schematics.

---
+++ Spire of Remembrance/Assets/Scripts/Props/Schematics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schematics : Interactable
+{
+	[SerializeField] private Sprite mapSprite;
+	[SerializeField] private MapScreen mapScreen;
+
+	public override Interaction[] interactions
+	{
+		get
+		{
+			return new Interaction[]
+			{
+				new Interaction("Copy Map", FillInMap, true)
+			};
+		}
+	}
+
+	private void FillInMap()
+	{
+		mapScreen.mapSprite = mapSprite;
+		Destroy(this);
+	}
+}
---
+++ Spire of Remembrance/Assets/Scripts/UI/MapScreen.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapScreen : MonoBehaviour
+{
+	#region Editor Fields
+
+	[SerializeField] private Image mapImage;
+	[SerializeField] private float zoomStep;
+	[SerializeField] private float maxZoom;
+	[SerializeField] private float minZoom;
+	[SerializeField] private Color blankColor;
+
+	#endregion // Editor Fields
+
+	#region Properties
+
+	public Sprite mapSprite
+	{
+		get
+		{
+			return mapImage.sprite;
+		}
+		set
+		{
+			mapImage.sprite = value;
+			if (value == null)
+			{
+				mapImage.color = blankColor;
+			}
+			else
+			{
+				mapImage.preserveAspect = true;
+				mapImage.color = Color.white;
+			}
+		}
+	}
+
+	#endregion // Properties
+
+	#region Unity Functions
+
+	private void Start()
+	{
+		mapSprite = mapSprite;
+	}
+
+	private void OnEnable()
+	{
+		mapImage.transform.localScale = Vector3.one;
+	}
+
+	#endregion // Unity Function
+
+	#region Public Functions
+
+	public void ZoomIn()
+	{
+		float zoom = Mathf.Min(mapImage.transform.localScale.x + zoomStep, maxZoom);
+		mapImage.transform.localScale = Vector3.one * zoom;
+	}
+
+	public void ZoomOut()
+	{
+		float zoom = Mathf.Max(mapImage.transform.localScale.y - zoomStep, minZoom);
+		mapImage.transform.localScale = Vector3.one * zoom;
+	}
+
+	#endregion // Public Functions
+}
